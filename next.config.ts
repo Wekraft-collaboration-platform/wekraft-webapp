2005a966ba17e1f7bf069027f7e3e07aee4ef0a3
ronitrai27 <108011380+ronitrai27@users.noreply.github.com>
2026-03-10 01:30:13 +0530

    admin added and env failed...

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,9 @@ const nextConfig: NextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
   },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
+  // compiler: {
+  //   removeConsole: process.env.NODE_ENV === "production",
+  // },
   poweredByHeader: false,
   compress: true,
   experimental: {
